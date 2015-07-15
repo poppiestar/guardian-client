05197c340708a8eda6322a4022dddf0eea9d46a9
Drew Miller <drew@onpatrol.co>
2015-07-16 00:21:29 +0100

    Adding a rough list of activities and displaying them on the user dashboard

---
+++ activities.js
@@ -0,0 +1,182 @@
+
+'use strict';
+
+module.exports = [
+    {
+        "name": "Crota’s End - Normal Mode",
+        "id": "crota:nm"
+    },
+    {
+        "name": "Crota’s End - Hard Mode",
+        "id": "crota:hm"
+    },
+    {
+        "name": "Vault of Glass - Normal Mode",
+        "id": "vog:nm"
+    },
+    {
+        "name": "Vault of Glass - Hard Mode",
+        "id": "vog:nm"
+    },
+    {
+        "name": "Prison of Elders - Level 28",
+        "id": "poe:28"
+    },
+    {
+        "name": "Prison of Elders - Level 32",
+        "id": "poe:32"
+    },
+    {
+        "name": "Prison of Elders - Level 34",
+        "id": "poe:34"
+    },
+    {
+        "name": "Prison of Elders - Level 35",
+        "id": "poe:35"
+    },
+    {
+        "name": "Trials of Osiris",
+        "id": "crucible:trials"
+    },
+    {
+        "name": "Weekly Nightfall Strike",
+        "id": "nightfall"
+    },
+    {
+        "name": "Weekly Heroic Strike - Level 26",
+        "id": "weekly:26"
+    },
+    {
+        "name": "Weekly Heroic Strike - Level 28",
+        "id": "weekly:28"
+    },
+    {
+        "name": "Weekly Heroic Strike - Level 30",
+        "id": "weekly:30"
+    },
+    {
+        "name": "Weekly Heroic Strike - Level 32",
+        "id": "weekly:32"
+    },
+    {
+        "name": "Strike Playlist - Eagle (18)",
+        "id": "strike:eagle"
+    },
+    {
+        "name": "Strike Playlist - Viper (20)",
+        "id": "strike:viper"
+    },
+    {
+        "name": "Strike Playlist - Wolf (22)",
+        "id": "strike:wolf"
+    },
+    {
+        "name": "Strike Playlist - Tiger (24)",
+        "id": "strike:tiger"
+    },
+    {
+        "name": "Strike Playlist - Roc (26)",
+        "id": "strike:roc"
+    },
+    {
+        "name": "Strike Playlist - Dragon (28)",
+        "id": "strike:dragon"
+    },
+    {
+        "name": "Strike - The Shadow Thief",
+        "id": "strike:shadow-thief"
+    },
+    {
+        "name": "Strike - The Devil’s Lair",
+        "id": "strike:devils-lair"
+    },
+    {
+        "name": "Strike - The Summoning Pits",
+        "id": "strike:summoning-pits"
+    },
+    {
+        "name": "Strike - The Nexus",
+        "id": "strike:nexus"
+    },
+    {
+        "name": "Strike - Winter’s Run",
+        "id": "strike:winters-run"
+    },
+    {
+        "name": "Strike - Cerberus Vae III",
+        "id": "strike:cerverus-vae"
+    },
+    {
+        "name": "Strike - Dust Palace",
+        "id": "strike:dust-palace"
+    },
+    {
+        "name": "Strike - The Undying Mind",
+        "id": "strike:undying-mind"
+    },
+    {
+        "name": "Strike - The Will of Crota",
+        "id": "strike:will-of-crota"
+    },
+    {
+        "name": "Story Mission",
+        "id": "story:mission"
+    },
+    {
+        "name": "Daily Heroic Story - Level 22",
+        "id": "daily:22"
+    },
+    {
+        "name": "Daily Heroic Story - Level 26",
+        "id": "daily:26"
+    },
+    {
+        "name": "Daily Heroic Story - Level 28",
+        "id": "daily:26"
+    },
+    {
+        "name": "Daily Heroic Story - Level 30",
+        "id": "daily:30"
+    },
+    {
+        "name": "Daily Heroic Story - Level 32",
+        "id": "daily:32"
+    },
+    {
+        "name": "Bounties",
+        "id": "bounties"
+    },
+    {
+        "name": "Patrol",
+        "id": "patrol"
+    },
+    {
+        "name": "Crucible - 3 Person",
+        "id": "crucible:3-person"
+    },
+    {
+        "name": "Crucible - 6 Person",
+        "id": "crucible:6-person"
+    },
+    {
+        "name": "Crucible - Doubles Skirmish",
+        "id": "crucible:doubles-skirmish"
+    },
+    {
+        "name": "Crucible - Iron Banner",
+        "id": "crucible:iron-banner"
+    },
+    {
+        "name": "Crucible - Salvage",
+        "id": "crucible:salvage"
+    },
+    {
+        "name": "Exotic Weapon Bounty",
+        "id": "exotic"
+    },
+    {
+        "name": "Queen’s Kill Order",
+        "id": "queen"
+    }
+];
+
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var Path = require('path');
 var Hapi = require('hapi');
 var Good = require('good');
 
+var allActivities = require('./activities');
+
 var server = new Hapi.Server();
 
 server.connection({ port: 4000 });
@@ -30,15 +32,42 @@ server.route({
     method: 'POST',
     path: '/login',
     handler: function (request, reply) {
+        // on success, get user status
+
+        // redirect to dashboard if status exists,
+        // redirect to start wizard if status doesn't exist
         reply.view('login');
     }
 });
+
+function prepareActivities (guardian) {
+    var activities = [];
+
+    for (var activity in allActivities) {
+        var thing = allActivities[activity];
+
+        activities.push({
+            name: thing.name,
+            id: thing.id,
+            available: guardian.hasOwnProperty(thing.id) ? guardian[thing.id] : false
+        });
+    }
+
+    return activities;
+}
 
 server.route({
     method: 'GET',
     path: '/dashboard',
     handler: function (request, reply) {
-        reply.view('dashboard');
+        reply.view('dashboard', {
+            username: 'poppiestar95',
+            status: 'Looking for a Black Hammer, Crota NM anyone?',
+            activities: prepareActivities({
+                'patrol': true,
+                'nightfall': false
+            })
+        });
     }
 });
 
